96f126f674b669c6cd6c88488787e9620fe1a572
oeiro <59517319+oeiro@users.noreply.github.com>
2020-02-08 12:27:56 -0500

    Initial Commit

---
+++ Persistance/APIProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+namespace Persistance
+{
+	class APIProgram
+	{
+		#region Constructors
+		public APIProgram(string name)
+		{
+			Name = name;
+		}
+		#endregion
+
+		#region Methods
+		public void Quit()
+		{
+			Running = false;
+		}
+
+		override public string ToString() 
+		{
+			return Name;
+		}
+		#endregion
+
+		#region Properties
+		public string Name { get; set; }
+		public bool Running { get; set; } = true;
+		#endregion
+	}
+}
---
+++ Persistance/BinaryProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance // TODO Read bytes from file
+{
+	class BinaryProgram : APIProgram
+	{
+		#region Constructors
+		public BinaryProgram(string name) : base(name)
+		{
+
+		}
+		#endregion
+
+		#region Properties
+		public List<byte> Buffer { get; } = new List<byte>();
+		public byte pc { get; set; }
+		#endregion
+	}
+}
